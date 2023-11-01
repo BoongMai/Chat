6148958a851e2a0881d32b6b0fa302b54bd59f53
BoongMai <thanhduypham9@gmail.com>
2023-11-01 22:59:57 +0700

    feat: sidebar server image

--- src/components/navigation/navigation-sidebar.tsx
+++ src/components/navigation/navigation-sidebar.tsx
@@ -2,6 +2,11 @@ import { currentProfile } from '@/lib/current-profile';
 import { db } from '@/lib/db';
 import { redirect } from 'next/navigation';
 import { NavigationAction } from './navigation-action';
+import { Separator } from '@/components/ui/separator';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { NavigationItem } from './navigation-item';
+import { ModeToggle } from '@/components/mode-toggle';
+import { UserButton } from '@clerk/nextjs';
 
 export const NavigationSideBar = async () => {
   const profile = await currentProfile();
@@ -10,7 +15,7 @@ export const NavigationSideBar = async () => {
     return redirect('/');
   }
 
-  const servers = db.server.findMany({
+  const servers = await db.server.findMany({
     where: {
       members: {
         some: {
@@ -22,7 +27,23 @@ export const NavigationSideBar = async () => {
 
   return (
     <div className='space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3'>
-      <NavigationAction/>
+      <NavigationAction />
+      <Separator className='h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto' />
+      <ScrollArea className='flex-1 w-full'>
+        {servers.map((server) => {
+          const { name, id, imageUrl } = server;
+          return (
+            <div key={server.id}>
+              <NavigationItem name={name} id={id} imageUrl={imageUrl} />
+            </div>
+          );
+        })}
+      </ScrollArea>
+      <div className='pb-3 mt-auto flex items-center flex-col gap-y-4'> 
+        <ModeToggle/>
+        <UserButton/>
+      </div>
+     
     </div>
   );
 };
